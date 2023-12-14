350a4dd6b43a95b059467630d39037db7d5c78b5
DarkWolf-12 <dead.lovesagar@gmail.com>
2023-12-14 22:40:52 +0800

    a
    a
    
    Co-Authored-By: thers <24668975+Rabin22@users.noreply.github.com>

---
+++ Amazon clone/amazon/amazon.js
@@ -0,0 +1,7 @@
+function handleSelection() {
+         
+    let selectedValue = document.getElementById("fruitDropdown").value;
+
+    
+    document.getElementById("selectedFruit").innerHTML = '';
+}
