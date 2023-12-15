a84b221c2ce377f2c03e49ac104a4cbc296af570
DarkWolf-12 <dead.lovesagar@gmail.com>
2023-12-15 23:12:17 +0800

    a
    a
    
    Co-Authored-By: thers <24668975+Rabin22@users.noreply.github.com>

--- Amazon clone/amazon/amazon.js
+++ Amazon clone/amazon/amazon.js
@@ -5,3 +5,26 @@ function handleSelection() {
     
     document.getElementById("selectedFruit").innerHTML = '';
 }
+
+
+function toggleMenu() {
+    var sideMenu = document.getElementById("mySideMenu");
+    if (sideMenu.style.width === "250px") {
+        closeMenu();
+    } else {
+        openMenu();
+    }
+}
+
+function openMenu() {
+    document.getElementById("mySideMenu").style.width = "350px";
+    document.getElementById("menuButton").style.marginLeft = "360px";
+    document.getElementById("content").style.marginLeft = "250px";
+    
+}
+
+function closeMenu() {
+    document.getElementById("mySideMenu").style.width = "0";
+    document.getElementById("menuButton").style.marginLeft = "0";
+    document.getElementById("content").style.marginLeft = "0";
+}
