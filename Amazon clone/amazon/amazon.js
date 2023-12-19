798587dc2dec295a37fea3fc8a348e0dd4a61792
DarkWolf-12 <dead.lovesagar@gmail.com>
2023-12-19 23:06:03 +0800

    ama
    ama
    
    Co-Authored-By: thers <24668975+Rabin22@users.noreply.github.com>

--- Amazon clone/amazon/amazon.js
+++ Amazon clone/amazon/amazon.js
@@ -58,3 +58,30 @@ function changeSlide(direction) {
 
 // Change background every 5 seconds (5000 milliseconds)
 setInterval(() => changeSlide(1), 5000);
+
+
+//code for footer and back to top button
+function setLanguage(languageCode) {
+    // Implement the logic to set the selected language
+    console.log('Selected language:', languageCode);
+
+
+     // Show the "Back to Top" button when scrolling down
+     window.onscroll = function() {
+        showBackToTopButton();
+    };
+
+    function showBackToTopButton() {
+        var button = document.getElementById("back-to-top");
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            button.style.display = "block";
+        } else {
+            button.style.display = "none";
+        }
+    }
+
+    // Scroll to the top when the button is clicked
+    function scrollToTop() {
+        
+        document.documentElement.scrollTop = 0;  
+    }
